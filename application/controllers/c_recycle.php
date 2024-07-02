b122476367dcc8b0010602a7fb2bcc5b94142919
Ikhsanurfz <sanurfz@gmail.com>
2024-07-03 01:10:12 +0700

    ikhsanurfz

---
+++ application/controllers/c_recycle.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class c_recycle extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('m_recycle');
+    }
+
+    public function index() {
+        $data['data_daur_ulang'] = $this->m_recycle->get_all_recycles();
+        $this->load->view('v_recycle/index', $data);
+    }
+
+    public function tambah() {
+        // Logika untuk form tambah data
+    }
+
+    public function view($id) {
+        // Logika untuk tampilan detail data
+    }
+
+    public function edit($id) {
+        // Logika untuk form edit data
+    }
+
+    public function hapus($id) {
+        // Logika untuk menghapus data
+    }
+}
+?>
