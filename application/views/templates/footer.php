77149d604a94d1d2d57a2ba88a0d70300df5a7e0
Raqanzaa <142374468+Raqanzaa@users.noreply.github.com>
2024-07-03 16:38:58 +0700

    -

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,7 @@
 </div>
 <!-- /.content-wrapper -->
 
-<footer class="main-footer" style="margin-top: 63px;">
+<footer class="main-footer" style="margin-top: 62px;">
   <div class="float-right d-none d-sm-block">
     <!-- <b>Version</b> 3.2.0 -->
   </div>
