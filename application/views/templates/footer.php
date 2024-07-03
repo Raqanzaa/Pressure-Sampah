be7483878bfa4e19758dabb1a45c1a7021ce3906
Raqanzaa <142374468+Raqanzaa@users.noreply.github.com>
2024-07-03 13:20:52 +0700

    pembenaran sidebar

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,7 @@
 </div>
 <!-- /.content-wrapper -->
 
-<footer class="main-footer" style="margin-top: 64px;">
+<footer class="main-footer" style="margin-top: 63px;">
   <div class="float-right d-none d-sm-block">
     <!-- <b>Version</b> 3.2.0 -->
   </div>
