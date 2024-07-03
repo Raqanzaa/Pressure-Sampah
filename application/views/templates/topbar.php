af46977fcf4a4f51b1108f6f826e14972fa09412
Raqanzaa <142374468+Raqanzaa@users.noreply.github.com>
2024-07-03 07:29:19 +0700

    pembenaran conflict

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -20,17 +20,17 @@
                     <a href="#" class="nav-link">Contact</a>
                 </li>
             </ul>
-
-        <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto pb-2 justify-content-end">
-            <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#" role="button">
-                    <div class="user-panel d-flex align-items-center">
-                        <div class="image">
-                            <img src="<?php echo base_url();?>/assets/AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                        </div>
-                        <div class="info">
-                            <span class="d-block"><?php echo isset($user['full_name']) ? $user['full_name'] : 'User'; ?></span>
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto pb-2 justify-content-end">
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#" role="button">
+                        <div class="user-panel d-flex align-items-center">
+                            <div class="image">
+                                <img src="<?php echo base_url('/assets/AdminLTE-3.2.0/dist/img/user2-160x160.jpg'); ?>" class="img-circle elevation-2" alt="User Image">
+                            </div>
+                            <div class="info">
+                                <span class="d-block"><?php echo isset($user['full_name']) ? $user['full_name'] : ''; ?></span>
+                            </div>
                         </div>
                     </a>
                     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
