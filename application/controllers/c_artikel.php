b243814fb5e184287b9fe2bbc781ae47131b0c04
Ikhsanurfz <sanurfz@gmail.com>
2024-07-04 14:03:12 +0700

    ikhsanurfz

--- application/controllers/c_artikel.php
+++ application/controllers/c_artikel.php
@@ -1,81 +1,27 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_artikel extends CI_Controller {
+class c_artikel extends CI_Controller {
 
     public function __construct()
     {
         parent::__construct();
         $this->load->model('m_artikel');
-        $this->load->library('form_validation');
+        // Pastikan user sudah login
+        if (!$this->session->userdata('user_id')) {
+            redirect('auth/login');
+        }
     }
 
     public function index()
     {
-        $data['title'] = 'Daftar Artikel';
         $data['articles'] = $this->m_artikel->get_all_articles();
+        $data['title'] = 'Daftar Artikel';
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/topbar', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('v_artikel/index', $data);
         $this->load->view('templates/footer');
-    }
-
-    public function tambah()
-    {
-        $data['title'] = 'Tambah Artikel';
-
-        $this->form_validation->set_rules('judul', 'Judul', 'required');
-        $this->form_validation->set_rules('isi', 'Isi', 'required');
-        $this->form_validation->set_rules('author_id', 'Author', 'required');
-
-        if ($this->form_validation->run() === FALSE) {
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('templates/sidebar', $data);
-            $this->load->view('v_artikel/tambah', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $article_data = [
-                'judul' => $this->input->post('judul'),
-                'isi' => $this->input->post('isi'),
-                'author_id' => $this->input->post('author_id')
-            ];
-            $this->m_artikel->insert_article($article_data);
-            redirect('c_artikel/index');
-        }
-    }
-
-    public function edit($id)
-    {
-        $data['title'] = 'Edit Artikel';
-        $data['article'] = $this->m_artikel->get_article_by_id($id);
-
-        $this->form_validation->set_rules('judul', 'Judul', 'required');
-        $this->form_validation->set_rules('isi', 'Isi', 'required');
-        $this->form_validation->set_rules('author_id', 'Author', 'required');
-
-        if ($this->form_validation->run() === FALSE) {
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('templates/sidebar', $data);
-            $this->load->view('v_artikel/edit', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $article_data = [
-                'judul' => $this->input->post('judul'),
-                'isi' => $this->input->post('isi'),
-                'author_id' => $this->input->post('author_id')
-            ];
-            $this->m_artikel->update_article($id, $article_data);
-            redirect('c_artikel/index');
-        }
-    }
-
-    public function hapus($id)
-    {
-        $this->m_artikel->delete_article($id);
-        redirect('c_artikel/index');
     }
 }
--- application/models/m_artikel.php
+++ application/models/m_artikel.php
@@ -2,40 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class m_artikel extends CI_Model {
-
-    public function get_all_articles()
-    {
+    
+    public function get_all_articles() {
         $this->db->select('a.*, u.full_name as author_name');
         $this->db->from('artikel a');
-        $this->db->join('t_users u', 'a.author_id = u.id_users');
+        $this->db->join('t_users u', 'a.author_id = u.id');
         $query = $this->db->get();
         return $query->result();
     }
 
-    public function get_article_by_id($id)
-    {
-        $this->db->select('a.*, u.full_name as author_name');
-        $this->db->from('artikel a');
-        $this->db->join('t_users u', 'a.author_id = u.id_users');
-        $this->db->where('a.id_artikel', $id);
-        $query = $this->db->get();
-        return $query->row();
-    }
-
-    public function insert_article($data)
-    {
-        return $this->db->insert('artikel', $data);
-    }
-
-    public function update_article($id, $data)
-    {
-        $this->db->where('id_artikel', $id);
-        return $this->db->update('artikel', $data);
-    }
-
-    public function delete_article($id)
-    {
-        $this->db->where('id_artikel', $id);
-        return $this->db->delete('artikel');
-    }
+    // Metode lain di model ini
 }
