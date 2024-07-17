8769f335566da790c73dd5de746a93a02e0d075a
Raqanzaa <142374468+Raqanzaa@users.noreply.github.com>
2024-07-17 18:28:20 +0700

    fixing code

--- application/controllers/c_landing.php
+++ application/controllers/c_landing.php
@@ -1,20 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class c_landing extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
         $this->load->model('m_artikel');
         $this->load->model('m_landing');
-        $this->load->model('m_auth');
+        // $this->load->model('m_auth');
     }
 
     // Fungsi untuk menampilkan halaman landing
     public function index() {
-        // Dapatkan user ID dari sesi
-        $user_id = $this->session->userdata('user_id');
-        
         // Dapatkan data artikel
         $data['artikel'] = $this->m_artikel->get_all_articles();
         
@@ -24,13 +23,13 @@ class c_landing extends CI_Controller {
             $yearFilter = date('Y');
         }
         
-        // Dapatkan data statistik untuk user yang terakhir login
-        $data['tps'] = $this->m_landing->get_tps_count($user_id);
-        $data['jenis_sampah'] = $this->m_landing->get_jenis_sampah_count($user_id);
-        $data['sampah_dikumpulkan'] = $this->m_landing->get_sampah_dikumpulkan($user_id, $yearFilter);
-        $data['kategori_sampah'] = $this->m_landing->get_kategori_sampah_count($user_id);
-        $data['sampah_didaur_ulang'] = $this->m_landing->get_sampah_didaur_ulang($user_id, $yearFilter);
-        $data['residu'] = $this->m_landing->get_residu($user_id, $yearFilter);
+        // Dapatkan data statistik tanpa batasan user_id
+        $data['tps'] = $this->m_landing->get_tps_count();
+        $data['jenis_sampah'] = $this->m_landing->get_jenis_sampah_count();
+        $data['sampah_dikumpulkan'] = $this->m_landing->get_sampah_dikumpulkan();
+        $data['kategori_sampah'] = $this->m_landing->get_kategori_sampah_count();
+        $data['sampah_didaur_ulang'] = $this->m_landing->get_sampah_didaur_ulang();
+        $data['residu'] = $this->m_landing->get_residu();
 
         $data['yearFilter'] = $yearFilter; // Sertakan tahun filter ke view
 
@@ -39,4 +38,5 @@ class c_landing extends CI_Controller {
         $this->load->view('t_landing/footer.php');
     }
 }
+
 ?>
